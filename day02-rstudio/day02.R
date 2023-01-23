0acf13510588e7ba888f917e070268ab25614746
preferred_name <user@subdomain.domain>
2023-01-22 21:07:33 -0500

    Tried some calculations..

--- day02-rstudio/day02.R
+++ day02-rstudio/day02.R
@@ -1,3 +1,23 @@
 # Here are some simple calculations
 2 + 2
 (2 * 3)^2
+
+# Some calculations I've tried:
+
+a = 10
+b = 10
+sum = a + b
+sum
+
+Res = (a)^2 + (b)^2 + 2*a*b
+Res
+
+Final_Res = sqrt(Res)
+Final_Res
+
+# Checking some Condition:
+
+if (sum == Final_Res)
+{
+  print ("Hi there!! I am Saheli")
+}
